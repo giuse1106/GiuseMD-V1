2f7dc9dcc4bfe9a6be96cb3f38e4a8fb35c5a469
Giuse <perr.gius11@gmail.com>
2025-06-02 22:23:45 +0200

    Update test_ping.js

--- plugins/test_ping.js
+++ plugins/test_ping.js
@@ -3,7 +3,7 @@ import speed from "performance-now";
 let handler = async (m, { conn }) => {
   let ini_timestamp = speed();
   let ini_latensi = speed() - ini_timestamp;
-  let text_ping = `${ini_latensi.toFixed(4)}`;
+  let text_ping = `Ping del bot: ${ini_latensi.toFixed(4)}`;
   conn.reply(m.chat, text_ping);
 };
 handler.command = ["test"];
